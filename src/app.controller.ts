a09d1c3bc42f00ace5a7eba9caf09ab50ee97342
Yakov Litvin <Yakov.A.Litvin@gmail.com>
2023-11-03 16:10:59 +0300

    feat: add a dto and an endpoint (simplified)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
+// normally, DTO is in another file, this one is here to make the example brief
+class UpdateProjectNameParamsDto {
+  @ApiProperty({
+    description: 'Internal Id of project',
+    example: '10fa8784-7006-499d-b97a-a406901df2b8',
+  })
+  @IsNotEmpty()
+  @IsUUID()
+  projectId: string;
+
+  @ApiProperty({
+    description: 'The name that project should be renamed into',
+    example: 'The Best Project',
+  })
+  @IsNotEmpty()
+  @IsString()
+  newName: string;
+}
 
 @Controller()
 export class AppController {
@@ -8,5 +29,11 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // was Patch in the real world example; using Get for simplicity
+  @Get('/:projectId/name/:newName')
+  async rename(@Param() params: UpdateProjectNameParamsDto): Promise<string> {
+    return `got params.projectId equal to ${params.projectId}, params.newName equal to ${params.newName}`;
   }
 }
